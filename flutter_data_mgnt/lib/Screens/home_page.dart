6bc52495b867b770ea67c8e2264eb7d411166b13
Deodate <ajadeo04@gmail.com>
2025-02-21 12:45:57 +0200

    PROVIDER IMPLEMENTATION

--- flutter_data_mgnt/lib/Screens/home_page.dart
+++ flutter_data_mgnt/lib/Screens/home_page.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+import '../provider/todo_provider.dart';
+// Make sure to import your TodoProvider
+
+class HomePage extends StatefulWidget {  // Changed to StatefulWidget
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override  // Added @override
+  void initState() {
+    super.initState();  // Moved super.initState() inside initState
+    
+    // Wrap Provider call in a Future to avoid calling build context in initState
+    Future.microtask(() {
+      Provider.of<TodoProvider>(context, listen: false).getAllTodos();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Provider API'),
